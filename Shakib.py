fb61b274b538fa1cdfb440b4389c3fd0bfc5af48
Shakibur Rahman <107750386+SHAKIBUR-404@users.noreply.github.com>
2023-02-11 13:58:14 +0600

    Add files via upload

---
+++ Shakib.py
@@ -0,0 +1,15 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n \n \033[1;31mFOLLOW MY GITHUB")
+    os.system('xdg-open https://github.com/SHAKIBUR-404');time.sleep(2)   
+    from TATA import menu
+    menu()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
