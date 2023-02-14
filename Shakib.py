54ba8ab4cd2f4c7e1e3cb64f65757660dc3d22b6
Shakibur Rahman <107750386+SHAKIBUR-404@users.noreply.github.com>
2023-02-14 16:54:54 +0600

    Update Shakib.py

--- Shakib.py
+++ Shakib.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n \n \033[1;31mFOLLOW MY GITHUB")
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n \n ENJOY FREE TOLS \n \n \033[1;31mFOLLOW MY GITHUB")
     os.system('xdg-open https://github.com/SHAKIBUR-404');time.sleep(2)   
     from TATA import menu
     menu()
