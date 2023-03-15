772e33799d6d31b29122b141d981ed111e529d0c
Shakibur Rahman <107750386+SHAKIBUR-404@users.noreply.github.com>
2023-03-15 18:29:16 +0600

    Update Shakib.py

--- Shakib.py
+++ Shakib.py
@@ -9,7 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n \n ENJOY FREE TOLS \n \n \033[1;31mFOLLOW MY GITHUB")
     os.system('xdg-open https://github.com/SHAKIBUR-404');time.sleep(2)   
-    from TATA import menu
+    from SHAKIB import menu
     menu()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
